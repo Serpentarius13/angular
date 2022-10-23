37b926b66bfa52e31379692d05701649a589b95c
Serpentarius13 <serpentarium13@mail.ru>
2022-10-23 19:06:42 +0300

    cards finished

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Tree',
+      imageUrl: 'assets/images/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this tree today',
+    },
+    {
+      title: 'Bike',
+      imageUrl: 'assets/images/biking.jpeg',
+      username: 'autos',
+      content: 'I rode a bike today',
+    },
+    {
+      title: 'Mountain',
+      imageUrl: 'assets/images/mountain.jpeg',
+      username: 'mount',
+      content: 'I climbed a mountain today',
+    },
+  ];
 }
